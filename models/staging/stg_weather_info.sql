0b1a83396b77ab4ded39f916c8cb891320d0c78e
Manoj Lobo <11038685@stud.hochschule-heidelberg.de>
2025-05-04 15:13:32 +0000

    added weather enrichment

---
+++ models/staging/stg_weather_info.sql
@@ -0,0 +1,36 @@
+with 
+
+source as (
+
+    select * from {{ source('cricket_raw', 'weather_info') }}
+
+),
+
+renamed as (
+
+    select
+        season,
+        match_id,
+        city,
+        match_num,
+        venue,
+        match_date,
+        match_time,
+        team1,
+        team2,
+        venue_id,
+        latitude,
+        longitude,
+        datetime,
+        temp_c,
+        `humidity_%`       AS humidity_percent,
+        pressure_hpa,
+        `cloudcover_%`     AS cloudcover_percent,
+        rain_mm,
+        wind_m_s
+
+    from source
+
+)
+
+select * from renamed
